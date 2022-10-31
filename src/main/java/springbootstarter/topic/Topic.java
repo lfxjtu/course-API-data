9cdcb95718b780aeb731920ea8e8f0b3ce92ef99
Fang Liu <lfxjtu@gmail.com>
2022-10-31 21:11:50 +1300

    init commit

---
+++ src/main/java/springbootstarter/topic/Topic.java
@@ -0,0 +1,30 @@
+package springbootstarter.topic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+
+@Entity
+public class Topic {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+    public Topic() {
+    }
+
+    public Topic(String id, String name, String description) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+}
